e78ee03db58c4482f213a8beff62df6d36eea289
Maxutka00 <0968535911max@gmail.com>
2023-08-08 00:32:30 +0300

    upd

--- utils/pyro_filters.py
+++ utils/pyro_filters.py
@@ -3,8 +3,8 @@ from pyrogram.types import Message
 
 
 async def empty_chat(flt, app: Client, message: Message):
-    if message.chat.id == 357405415:
-        return True
+    # if message.chat.id == 357405415:
+    #    return True
     if (await app.search_messages_count(message.chat.id)) == 1:
         return True
     return False
